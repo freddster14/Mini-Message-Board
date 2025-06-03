e181f648cebd89832c5576a5f264c08b778f2421
freddster14 <freddycarrillo951@gmail.com>
2025-06-03 10:09:24 -0500

    Add CSS styling and static file serving for improved UI

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require('node:path');
 const { indexRouter } = require('./routes');
 
 const app = express();
+const assestsPath = path.join(__dirname, 'public');
 const PORT = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(assestsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 
