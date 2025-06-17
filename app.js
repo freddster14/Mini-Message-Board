8363c653c596c7545ceaee5629ab9d5ab5a1291c
freddster14 <freddycarrillo951@gmail.com>
2025-06-17 11:28:16 -0500

    Add favicon support, update dependencies, and modify .gitignore

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('node:path');
+const favicon = require('serve-favicon');
+
 const { indexRouter } = require('./routes');
 
 const app = express();
@@ -9,6 +11,7 @@ const PORT = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(assestsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
